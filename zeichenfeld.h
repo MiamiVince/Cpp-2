9e791e2513daf9f03c6baf899d9a36d277d5f374
MiamiVince <34132866+MiamiVince@users.noreply.github.com>
2018-09-26 16:55:12 +0200

    Update zeichenfeld.h

--- zeichenfeld.h
+++ zeichenfeld.h
@@ -3,16 +3,25 @@
 
 
 #include <QWidget>
+#include <QTimer>
 
 class zeichenFeld : public QWidget
 {
-    Q_OBJECT
 public:
     zeichenFeld(QWidget *parent = 0);
 
+    void start(void) { timer->start(10); increment=1; };
+    void stop(void) { timer->stop(); increment=0; };
+
+private:
+    int x;
+    QTimer* timer;
+    int punkteStand;
+    int increment;
+
 protected:
     void paintEvent(QPaintEvent *event);
-
+    void keyPressEvent(QKeyEvent* event);
 
 };
 
