562f2b7810981d37e76e05466009c82d2fc6f5ab
MiamiVince <34132866+MiamiVince@users.noreply.github.com>
2018-09-26 16:54:41 +0200

    Update meinwidget.cpp

--- meinwidget.cpp
+++ meinwidget.cpp
@@ -7,9 +7,13 @@
 meinWidget::meinWidget(QWidget *parent)
     : QWidget(parent)
 {
-    QPushButton *startPause = new QPushButton(tr("Start"));
-    startPause->setFont(QFont("Times", 18, QFont::Bold));
-    connect(startPause, SIGNAL(clicked()), qApp, SLOT(quit()));
+    QPushButton *start = new QPushButton(tr("Start"));
+    start->setFont(QFont("Times", 18, QFont::Bold));
+    connect(start, SIGNAL(clicked()), qApp, SLOT(start()));
+
+    QPushButton *stop = new QPushButton(tr("Close"));
+    stop->setFont(QFont("Times", 18, QFont::Bold));
+    connect(stop, SIGNAL(clicked()), this, SLOT(quit()));
 
     QPushButton *save = new QPushButton(tr("Speichern"));
     save->setFont(QFont("Times", 18, QFont::Bold));
@@ -22,10 +26,21 @@ meinWidget::meinWidget(QWidget *parent)
     zeichenFeld *meinZeichenFeld = new zeichenFeld;
 
     QGridLayout *gridLayout = new QGridLayout;
-    gridLayout->addWidget(startPause, 0, 0);
-    gridLayout->addWidget(save, 1, 0);
-    gridLayout->addWidget(load, 2, 0);
+    gridLayout->addWidget(start, 0, 0);
+    gridLayout->addWidget(stop, 1, 0);
+    gridLayout->addWidget(save, 2, 0);
+    gridLayout->addWidget(load, 3, 0);
     gridLayout->addWidget(meinZeichenFeld, 0, 1, 5, 1);
     gridLayout->setColumnStretch(1, 10);
     setLayout(gridLayout);
+}
+
+void meinWidget::start(void)
+{
+    meinZeichenFeld->start();
+}
+
+void meinWidget::stop(void)
+{
+    meinZeichenFeld->stop();
 }
