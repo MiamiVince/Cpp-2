c20067fb9db18c30b1e02bd8b21f0b3500939358
MiamiVince <34132866+MiamiVince@users.noreply.github.com>
2018-09-26 16:55:38 +0200

    Update meinwidget.h

--- meinwidget.h
+++ meinwidget.h
@@ -1,16 +1,22 @@
 #ifndef MEINWIDGET_H
 #define MEINWIDGET_H
 
+#include "zeichenFeld.h"
 #include <QWidget>
 
 
 class meinWidget : public QWidget
 {
-
+    Q_OBJECT
 public:
     meinWidget(QWidget *parent = 0);
 
+private:
+    zeichenFeld *meinZeichenFeld;
+
+private slots:
+    void start(void);
+    void stop(void);
 
 };
-
 #endif // MEINWIDGET_H
