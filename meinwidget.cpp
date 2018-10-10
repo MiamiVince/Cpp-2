f9a51d3e36001dd9b372abbf9c86fb645db42f2f
MiamiVince <34132866+MiamiVince@users.noreply.github.com>
2018-10-10 19:26:44 +0200

    Update meinwidget.cpp

--- meinwidget.cpp
+++ meinwidget.cpp
@@ -3,11 +3,14 @@
 #include <QGridLayout>
 #include "meinWidget.h"
 #include "zeichenFeld.h"
+#include <QFile>
+#include <QMessageBox>
+#include <QFileDialog>
 
 meinWidget::meinWidget(QWidget* parent)
-    : QWidget(parent)
-{
+    : QWidget(parent) {
 
+    //erstellen der vier Buttons
     starter->setFont(QFont("Times", 18, QFont::Bold));
     connect(starter, SIGNAL(clicked()), this, SLOT(start()));
 
@@ -23,9 +26,11 @@ meinWidget::meinWidget(QWidget* parent)
     loader->setFont(QFont("Times", 18, QFont::Bold));
     connect(loader, SIGNAL(clicked()), this, SLOT(loadFile()));
 
+    //erstellen eines Zeichenfeldes
     meinZeichenFeld = new zeichenFeld;
     meinZeichenFeld->setFocusPolicy(Qt::StrongFocus);
 
+    //erstellen des gridlayouts und belegen der einzelnen widgets
     QGridLayout* gridLayout = new QGridLayout;
     gridLayout->addWidget(starter, 0, 0);
     gridLayout->addWidget(saver, 1, 0);
@@ -36,8 +41,12 @@ meinWidget::meinWidget(QWidget* parent)
     setLayout(gridLayout);
 }
 
-void meinWidget::start(void)
-{
+/*
+ Die Methode start() startet das Spiel.
+ Über eine boolean Abfrage wird die Beschriftung des Start-Buttons geändert.
+ Danach kann über den gleichen Button das Spiel pausiert werden.
+*/
+void meinWidget::start(void){
     if(startPause == true){
         meinZeichenFeld->start();
         startPause = false;
@@ -50,3 +59,47 @@ void meinWidget::start(void)
     }
 }
 void meinWidget::stop(void){}
+
+// Die Methode saveFile() macht das Speichern eines Savegames möglich.
+void meinWidget::saveFile(void){
+    if (meinZeichenFeld->getPunkteStand() == 0){
+        QMessageBox::warning(this, tr("Fehler"), tr("Du musst das Spiel erst starten!"), QMessageBox::Ok);
+        return;
+    }
+    QFileDialog dialog(this);
+    QString fileName;
+    QFile file;
+
+    dialog.setFileMode(QFileDialog::AnyFile);
+    fileName = dialog.getSaveFileName(this,tr("Speichern als"), ".", tr("Savegame-File (*.savegame)"));
+
+
+    if (fileName.isNull() == false){
+        file.setFileName(fileName);
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)){
+            QMessageBox::warning(this, tr("Dateifehler"), tr("Folgende Datei kann nicht verwendet werden: ") + fileName, QMessageBox::Ok);
+        }
+        meinZeichenFeld->serialize(file);
+        file.close();
+        return;
+    }
+}
+
+// Die Methode loadFile() macht das Laden eines Savegames möglich.
+void meinWidget::loadFile(void){
+    QFileDialog dialog(this);
+    QString fileName;
+    QFile file;
+
+    dialog.setFileMode(QFileDialog::AnyFile);
+    fileName = dialog.getOpenFileName(this, tr("Speichern als"), ".", tr("Savegame-File (*.savegame)"));
+    if (fileName.isNull() == false){
+        file.setFileName(fileName);
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+            QMessageBox::warning(this, tr("Dateifehler"), tr("Folgende Datei kann nicht geöffnet werden: ") + fileName, QMessageBox::Ok);
+        }
+        meinZeichenFeld->deserialize(file);
+        file.close();
+        return;
+    }
+}
