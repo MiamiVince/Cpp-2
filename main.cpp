887ea2247b4c2624c63ec74bdf7636c4b453dbcd
MiamiVince <34132866+MiamiVince@users.noreply.github.com>
2018-10-10 19:27:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "meinWidget.h"
-// Qt5 add:
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+
+/*
+    Clemens Haberl
+    Matrikelnummer: 7324637
+    C++ Semesterarbeit Teil 2 SoSe 2018
+    BM3: Softwaretechnologie
+    Prof. Dr. Eide
+    10.10.2018
+*/
     QApplication app(argc, argv);
     meinWidget widget;
     widget.setGeometry(100, 100, 500, 500);
